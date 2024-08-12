29dacb32557b21b8ff83f59a543b6e4ae72f4690
JorgeVilledaS <168128321+JorgeVilledaS@users.noreply.github.com>
2024-08-11 19:57:43 -0600

    Create Medicamento.java

---
+++ Medicamento.java
@@ -0,0 +1,56 @@
+public class Medicamento {
+    private String nombre;
+    private String descripcion;
+    private int dosis;
+    private LocalTime horarioSuministro;
+    private boolean recetado;
+    private float inventario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(int dosis) {
+        this.dosis = dosis;
+    }
+
+    public LocalTime getHorarioDeSuministro() {
+        return horarioSuministro;
+    }
+
+    public void setHorarioDeSuministro(LocalTime horarioSuministro) {
+        this.horarioSuministro = horarioSuministro;
+    }
+
+    public boolean isRecetado() {
+        return recetado;
+    }
+
+    public void setRecetado(boolean recetado) {
+        this.recetado = recetado;
+    }
+
+    public float getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(float inventario) {
+        this.inventario = inventario;
+    }
+}

76731b5ffd1dc83aeb74b964239665ca433900d7
JorgeVilledaS <168128321+JorgeVilledaS@users.noreply.github.com>
2024-08-11 19:57:20 -0600

    Create Paciente.java

---
+++ Paciente.java
@@ -0,0 +1,47 @@
+public class Paciente {
+    private String nombre;
+    private int edad;
+    private List<String> condiciones;
+    private List<Medicamento> medicamentos;
+    private String informacionAdicional;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public List<String> getCondiciones() {
+        return condiciones;
+    }
+
+    public void setCondiciones(List<String> condiciones) {
+        this.condiciones = condiciones;
+    }
+
+    public List<Medicamento> getMedicamentos() {
+        return medicamentos;
+    }
+
+    public void setMedicamentos(List<Medicamento> medicamentos) {
+        this.medicamentos = medicamentos;
+    }
+
+    public String getInformacionAdicional() {
+        return informacionAdicional;
+    }
+
+    public void setInformacionAdicional(String informacionAdicional) {
+        this.informacionAdicional = informacionAdicional;
+    }
+}

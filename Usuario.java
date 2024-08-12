60c109bb2e12f1e9986306dabb4072cebbf86524
JorgeVilledaS <168128321+JorgeVilledaS@users.noreply.github.com>
2024-08-11 19:52:35 -0600

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,56 @@
+public class Usuario {
+    private String nombre;
+    private String nombreUsuario;
+    private String contrasena;
+    private int edad;
+    private String sexo;
+    private String tipoUsuario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}

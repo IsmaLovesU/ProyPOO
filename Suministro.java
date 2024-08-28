c0b22a3bd47aebfdc829c4e274d1c9bce1d0d388
ESJ0 <usuarioj0s3p1@gmail.com>
2024-08-27 20:53:48 -0600

    Se agregaron los atributos a la clase suministro

---
+++ Suministro.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Suministro {
+    //Los atributos baby
+    private int id;
+    private String medicamentoMaloshPiumPium;
+    private int cantidad;
+    private LocalDate fechaSuministroUWU;
+    private LocalTime horaSuministroAÑAñ;
+    private String estado;
+}

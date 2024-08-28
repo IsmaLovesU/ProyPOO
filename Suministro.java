c11be9d6ab866d899c80f763fe1f4b62682f9ced
ESJ0 <usuarioj0s3p1@gmail.com>
2024-08-27 21:12:32 -0600

    Se le agregaron los gets y sets a la clase suministro

--- Suministro.java
+++ Suministro.java
@@ -20,4 +20,53 @@ public class Suministro {
         this.horaSuministroAÑA = horaSuministroAÑA;
         this.estado = estado;
     }
+
+    //Gettes y setters (Me quede sin imaginacion)
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+    public String getMedicamento() {
+        return medicamentoMaloshPiumPium;
+    }
+
+    public void setMedicamento(String medicamentoMaloshPiumPium) {
+        this.medicamentoMaloshPiumPium = medicamentoMaloshPiumPium;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public LocalDate getFechaSuministro() {
+        return fechaSuministroUWU;
+    }
+
+    public void setFechaSuministro(LocalDate fechaSuministroUWU) {
+        this.fechaSuministroUWU = fechaSuministroUWU;
+    }
+
+    public LocalTime getHoraSuministro() {
+        return horaSuministroAÑA;
+    }
+
+    public void setHoraSuministro(LocalTime horaSuministroAÑA) {
+        this.horaSuministroAÑA = horaSuministroAÑA;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }

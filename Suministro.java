cc9eb5f94d7ee8b1f8e7d8079f5153cc3ef0538d
ESJ0 <usuarioj0s3p1@gmail.com>
2024-08-27 21:01:48 -0600

    Se le agrego el metodo constructor a la clase suministro

--- Suministro.java
+++ Suministro.java
@@ -7,6 +7,17 @@ public class Suministro {
     private String medicamentoMaloshPiumPium;
     private int cantidad;
     private LocalDate fechaSuministroUWU;
-    private LocalTime horaSuministroAÑAñ;
+    private LocalTime horaSuministroAÑA;
     private String estado;
+
+
+    //El bob constructor
+    public Suministro(int id, String medicamentoMaloshPiumPium, int cantidad, LocalDate fechaSuministroUWU, LocalTime horaSuministroAÑA, String estado){
+        this.id = id;
+        this.medicamentoMaloshPiumPium = medicamentoMaloshPiumPium;
+        this.cantidad = cantidad;
+        this.fechaSuministroUWU = fechaSuministroUWU;
+        this.horaSuministroAÑA = horaSuministroAÑA;
+        this.estado = estado;
+    }
 }

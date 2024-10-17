c06fd53a69d8102a3446ffc0ec5649296f3753e7
Andrés Ismalej <andesteisma@gmail.com>
2024-10-17 04:40:08 -0600

    Cambio de return en metodo de InicioSesion a booleanos y agregacion de la clase GeneradorId

---
+++ GeneradorId.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/**
+ * Universidad del Valle de Gutemala
+ * Programación Orinetada a Objetos 
+ * Sección: 10
+ * Ing. Kimberly Barrera
+ * Proyecto - Pillas
+*/
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Clase GeneradorId se encarga de generar IDs únicos de 6 caracteres para los usuarios.
+ * 25!/19! de probabilidad que se repita algun ID.
+ */
+class GeneradorId {
+    private static ArrayList<String> idsGenerados = new ArrayList<>();
+    private static final String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /**
+     * Genera un ID único y se que no se repita.
+     * @return El ID generado.
+     */
+    public static String generarId(){
+        Random random = new Random();
+        String idGenerado;
+        boolean idUnico;
+
+        do{
+            idGenerado = "";
+            for (int i = 0; i < 6; i++) {
+                int indiceAleatorio = random.nextInt(letras.length());
+                idGenerado += letras.charAt(indiceAleatorio);
+            }
+
+            //Comprobacion del ID.
+            idUnico = !idsGenerados.contains(idGenerado);
+        } while (!idUnico);
+        idsGenerados.add(idGenerado);
+
+        return idGenerado; 
+    }
+}
